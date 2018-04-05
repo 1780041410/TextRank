5fce82776d5dc8cab963e3fcf7b93dd46427e90c
1780041410 <32005041+1780041410@users.noreply.github.com>
2018-04-05 20:19:06 +0800

    关键词提取
    关键词提取

---
+++ TextRank4ZH-master/test/jieba_test.py
@@ -0,0 +1,13 @@
+#-*- encoding:utf-8 -*-
+from __future__ import print_function
+
+import sys
+
+
+import jieba.posseg as pseg
+words = pseg.cut("我爱北京天安门.。；‘你的#")
+for w in words:
+    # print(w.word)
+    print('{0} {1}'.format(w.word, w.flag))
+    print(type(w.word))  # in py2 is unicode, py3 is str
+
